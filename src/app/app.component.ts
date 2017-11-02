b8add6783e8c1506f500a21e7034484ceea6c48f
ctdoan33 <doanchristopher_1@yahoo.com>
2017-11-01 19:36:50 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switches = ["ON","ON","ON","ON","ON","ON","ON","ON","ON","ON"];
+  switch = function(index){
+	  if(this.switches[index]=="ON"){
+		this.switches[index]="OFF";
+	  }else if(this.switches[index]=="OFF"){
+		this.switches[index]="ON"
+	  }
+  }
 }
